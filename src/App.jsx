a27b02549b04eff50cfd91342cb1d7b2510f8d40
Souru-Hishinuma <105961234+Souru-Hishinuma@users.noreply.github.com>
2022-05-28 14:59:29 +0900

    jsx構造作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>ああああ</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <p>完了済みのTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>aaaa</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
